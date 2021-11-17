4e658006baa240149d9cd1b634de59c090c34114
tammamakhan <tammam.a.khan@gmail.com>
2021-11-16 20:28:41 -0800

    Created Form component and began implementing LoginForm component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="content">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Redirect from="/" to="/login" />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Joi from "joi-browser";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+}
+
+export default LoginForm;
